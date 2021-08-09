0df2c57e0e94c49050970589cfb0557209576d71
fullstackboss <romeltejada@gmail.com>
2021-08-09 06:11:41 -0500

    Comiezo de la Documentacion

---
+++ 1_tipos_variables.dart
@@ -0,0 +1,25 @@
+//Tipos de Variables
+void main() {
+  var nada;
+  String nombre="kakaroto";
+  int numero=123;
+  double doble=10.5;
+  bool boleano=true;
+  
+  List milista = ['juan', 'pedro', 'lupin'];
+  
+  Map mimapa = {
+  'nombre': 'vegeta',
+  'edad': '35',
+  'sexo': 'masculino'
+  };
+  
+  print(nada.runtimeType);
+  print(nombre);
+  print(numero.toString());
+  print(doble.toString());
+  print(boleano.toString());
+  print(milista[1]);
+  print(mimapa['nombre']);
+  
+}
